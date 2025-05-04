09fe45a566c04eb8bcd6d3d62022c855e5e586d5
qinran6271 <qinranwa@buffalo.edu>
2025-05-04 01:56:48 -0400

    Update index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -2,6 +2,7 @@
 import serverless from 'serverless-http';
 import app        from '../app.mjs';
 
+
 export default serverless(app, {
-  provider: { callbackWaitsForEmptyEventLoop: false }
+  callbackWaitsForEmptyEventLoop: false
 });
