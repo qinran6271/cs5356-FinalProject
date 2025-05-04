148a193465da5d8fe499e75721d648aa48267aa2
qinran6271 <qinranwa@buffalo.edu>
2025-05-04 02:19:34 -0400

    Update app_test.mjs

--- app_test.mjs
+++ app_test.mjs
@@ -1,5 +1,11 @@
+// app_test.mjs
+import express from 'express';
+
+const app = express();
+
+// 最简单的根路由
 app.get('/', (req, res) => {
-    res.send('Hello World');
-  });
-  
-  export default app;
+  res.send('Hello World');
+});
+
+export default app;
