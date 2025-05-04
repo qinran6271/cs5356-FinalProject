05edbb14af7d73bcdca6c36b90a82ce1a91ea91c
qinran6271 <qinranwa@buffalo.edu>
2025-05-04 02:51:41 -0400

    try try

--- api/index.mjs
+++ api/index.mjs
@@ -1,21 +1,6 @@
-// // api/index.mjs
-// import serverless from 'serverless-http';
-// import app        from '../app_test.mjs';
+import app from '../app.mjs';
 
+export default app;
 
-// export default serverless(app, {
-//   callbackWaitsForEmptyEventLoop: false
-// });
-// api/index.mjs
-// api/index.mjs
-import serverless from 'serverless-http';
-import express    from 'express';
-
-const app = express();
-app.get('/', (req, res) => res.send('Hello Express'));
-
-export default serverless(app, {
-  callbackWaitsForEmptyEventLoop: false
-});
 
   
