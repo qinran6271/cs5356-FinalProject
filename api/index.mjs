3364cd65498669784da4b42f8dfa7fc8651c8414
qinran6271 <qinranwa@buffalo.edu>
2025-05-04 02:27:14 -0400

    test vercel

--- api/index.mjs
+++ api/index.mjs
@@ -1,8 +1,13 @@
+// // api/index.mjs
+// import serverless from 'serverless-http';
+// import app        from '../app_test.mjs';
+
+
+// export default serverless(app, {
+//   callbackWaitsForEmptyEventLoop: false
+// });
 // api/index.mjs
-import serverless from 'serverless-http';
-import app        from '../app_test.mjs';
-
-
-export default serverless(app, {
-  callbackWaitsForEmptyEventLoop: false
-});
+export default function handler(req, res) {
+    return res.status(200).send('OK');
+  }
+  
